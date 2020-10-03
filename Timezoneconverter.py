a41254f71febd00741ec54b91e8c34dcbeee9569
12ankitsingh99 <52731346+12ankitsingh99@users.noreply.github.com>
2020-10-03 10:32:19 +0530

    Update Timezoneconverter.py

--- Timezoneconverter.py
+++ Timezoneconverter.py
@@ -2,18 +2,13 @@ import pytz, datetime
 
 year = int(input())
 month = int(input())
-day = int(input())
+date = int(input())
 hour = int(input())
 minute = int(input())
 
 #converting to a date
 
-users_time = datetime.datetime(year,month,day,hour,minute)
-
-delhi_timezone = pytz.timezone('Asia/Kolkata')
-tokyo_timezone = pytz.timezone('Asia/Tokyo')
-london_timezone = pytz.timezone('UTC')
-sydney_timezone = pytz.timezone('Australia/Sydney')
+users_time = datetime.datetime(year,month,date,hour,minute)
 
 kolkata_timezone = pytz.timezone('Asia/Kolkata')
 tokyo_timezone = pytz.timezone('Asia/Tokyo')
@@ -21,22 +16,13 @@ london_timezone = pytz.timezone('UTC')
 sydney_timezone = pytz.timezone('Australia/Sydney')
 
 
-delhi_time = pytz.utc.localize(users_time).astimezone(delhi_timezone)
+kolkata_time = pytz.utc.localize(users_time).astimezone(kolkata_timezone)
 tokyo_time = pytz.utc.localize(users_time).astimezone(tokyo_timezone)
 london_time = pytz.utc.localize(users_time).astimezone(london_timezone)
 sydney_time = pytz.utc.localize(users_time).astimezone(sydney_timezone)
 
-delhi_time = pytz.utc.localize(users_time).astimezone(delhi_timezone)
-tokyo_time = pytz.utc.localize(users_time).astimezone(tokyo_timezone)
-london_time = pytz.utc.localize(users_time).astimezone(london_timezone)
-sysdney_time = pytz.utc.localize(users_time).astimezone(sydney_timezone)
 
-print("New Delhi Time is", delhi_time.isoformat())
-print("Tokyo Time is", tokyo_time.isoformat())
-print("London Time is", london_time.isoformat())
-print("Sydney Time is", sydney_time.isoformat())
-
-print("New Delhi Time is", bihar_time.isoformat())
-print("Tokyo Time is", mumbai_time.isoformat())
-print("London Time is", pune_time.isoformat())
-print("Sydney Time is", punjab_time.isoformat())
+print("Kolkata Time is -", kolkata_time.isoformat())
+print("Tokyo Time is -", tokyo_time.isoformat())
+print("London Time is -", london_time.isoformat())
+print("Sydney Time is -", sydney_time.isoformat())
