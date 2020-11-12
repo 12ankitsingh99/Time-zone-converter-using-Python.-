7d90c9e886f54aeee7c1893de6dbb36a6f26be4e
12ankitsingh99 <52731346+12ankitsingh99@users.noreply.github.com>
2020-11-12 22:06:51 +0530

    Update Timezoneconverter.py

--- Timezoneconverter.py
+++ Timezoneconverter.py
@@ -1,28 +1,44 @@
-import pytz, datetime
+from datetime import datetime 
+import pytz 
 
-year = int(input())
-month = int(input())
-date = int(input())
-hour = int(input())
-minute = int(input())
+UTC = pytz.utc 
 
-#converting to a date
+timeZ_Kl = pytz.timezone('Asia/Kolkata') 
+timeZ_Ny = pytz.timezone('America/New_York') 
+timeZ_Ma = pytz.timezone('Africa/Maseru') 
+timeZ_Ce = pytz.timezone('US/Central') 
+timeZ_At = pytz.timezone('Europe/Athens') 
 
-users_time = datetime.datetime(year,month,date,hour,minute)
+dt_Kl = datetime.now(timeZ_Kl) 
+dt_Ny = datetime.now(timeZ_Ny) 
+dt_Ma = datetime.now(timeZ_Ma) 
+dt_Ce = datetime.now(timeZ_Ce) 
+dt_At = datetime.now(timeZ_At) 
 
-kolkata_timezone = pytz.timezone('Asia/Kolkata')
-tokyo_timezone = pytz.timezone('Asia/Tokyo')
-london_timezone = pytz.timezone('UTC')
-sydney_timezone = pytz.timezone('Australia/Sydney')
+utc_Kl = dt_Kl.astimezone(UTC) 
+utc_Ny = dt_Ny.astimezone(UTC) 
+utc_Ma = dt_Ma.astimezone(UTC) 
+utc_Ce = dt_Ce.astimezone(UTC) 
+utc_At = dt_At.astimezone(UTC) 
 
+print("UTC Format \t\t\t IST Format") 
 
-kolkata_time = pytz.utc.localize(users_time).astimezone(kolkata_timezone)
-tokyo_time = pytz.utc.localize(users_time).astimezone(tokyo_timezone)
-london_time = pytz.utc.localize(users_time).astimezone(london_timezone)
-sydney_time = pytz.utc.localize(users_time).astimezone(sydney_timezone)
+print(utc_Kl.strftime('%Y-%m-%d %H:%M:%S %Z %z'), 
+	"\t ", 
+	dt_Kl.strftime('%Y-%m-%d %H:%M:%S %Z %z')) 
 
+print(utc_Ny.strftime('%Y-%m-%d %H:%M:%S %Z %z'), 
+	"\t ", 
+	dt_Kl.strftime('%Y-%m-%d %H:%M:%S %Z %z')) 
 
-print("Kolkata Time is -", kolkata_time.isoformat())
-print("Tokyo Time is -", tokyo_time.isoformat())
-print("London Time is -", london_time.isoformat())
-print("Sydney Time is -", sydney_time.isoformat())
+print(utc_Ma.strftime('%Y-%m-%d %H:%M:%S %Z %z'), 
+	"\t ", 
+	dt_Kl.strftime('%Y-%m-%d %H:%M:%S %Z %z')) 
+
+print(utc_Ce.strftime('%Y-%m-%d %H:%M:%S %Z %z'), 
+	"\t ", 
+	dt_Kl.strftime('%Y-%m-%d %H:%M:%S %Z %z')) 
+
+print(utc_At.strftime('%Y-%m-%d %H:%M:%S %Z %z'), 
+	"\t ", 
+	dt_Kl.strftime('%Y-%m-%d %H:%M:%S %Z %z')) 
